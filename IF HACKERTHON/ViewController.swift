03e4da2a977ad3609f9a8cea07721fd803107e77
정재근 <jungjaegeun@jeongjaegeun-ui-macbookpro.local>
2018-10-23 22:34:37 +0900

    Initial Commit

---
+++ IF HACKERTHON/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IF HACKERTHON
+//
+//  Created by 정재근 on 23/10/2018.
+//  Copyright © 2018 개발하는인싸들. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
